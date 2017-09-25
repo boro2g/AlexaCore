3126ac70252ea54342db4bb2c42d0535d424be13
boro <boro2g@gmail.com>
2017-09-25 18:17:27 +0100

    Added tests for cookie persistence

--- AlexaCore.Tests/Content/ContentServiceTests.cs
+++ AlexaCore.Tests/Content/ContentServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using Alexa.NET.Request;
 using AlexaCore.Content;
@@ -14,25 +15,81 @@ namespace AlexaCore.Tests.Content
         [Test]
         public void WhenContentIsRequested_ValidTextIsReturned()
         {
-            var queue = new PersistentQueue<ApplicationParameter>(null,
-                new Session {Attributes = new Dictionary<string, object>()}, "key");
+            var queue = BuildQueue();
+
+            var responseText = "Welcome to the house of fun";
 
             var contentService = new TestContentService(queue, "userId",
-                new MockHttpClient(new HttpResponseMessage
-                {
-                    Content = new StringContent(
-                        JsonConvert.SerializeObject(new IntentContent
-                        {
-                            Content = "Welcome to the house of fun"
-                        }))
-                }));
+                new MockHttpClient(BuildResponseMessage(responseText)));
 
             var result = contentService.LoadAndFormatContent("HelpIntent", "default text");
 
-            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo(responseText));
+        }
 
-            //todo - need some tests around cookies and persistance
-            //todo - need to actually test it working
+        [Test]
+        public void WhenContentIsRequested_CookiesGetPersistedInQueue()
+        {
+            var queue = BuildQueue();
+
+            var aspNetSessionid = "ASP.NET_SessionId";
+
+            var cookieValue = "123";
+
+            var contentService = new TestContentService(queue, "userId",
+                new MockHttpClient(BuildResponseMessage("text",
+                    new[] {new CookieValue {Key = aspNetSessionid, Value = cookieValue}})));
+
+            contentService.LoadAndFormatContent("HelpIntent", "default text");
+
+            VerifyQueueContainsCookie(queue, aspNetSessionid, cookieValue);
+
+            contentService = new TestContentService(queue, "userId",
+                new MockHttpClient(BuildResponseMessage("text")));
+
+            contentService.LoadAndFormatContent("HelpIntent", "default text");
+
+            VerifyQueueContainsCookie(queue, aspNetSessionid, cookieValue);
+        }
+
+        private void VerifyQueueContainsCookie(PersistentQueue<ApplicationParameter> queue, string cookieName, string cookieValue)
+        {
+            var cookieEntry = queue.GetValue("__cookies");
+
+            Assert.That(cookieEntry, Is.Not.Null);
+
+            var cookieValues = JsonConvert.DeserializeObject<CookieValue[]>(cookieEntry);
+
+            var matchingCookie = cookieValues.FirstOrDefault(a => a.Key == cookieName);
+
+            Assert.That(matchingCookie, Is.Not.Null);
+
+            Assert.That(matchingCookie.Value, Is.EqualTo(cookieValue));
+        }
+
+        private static HttpResponseMessage BuildResponseMessage(string responseText, CookieValue[] cookies = null)
+        {
+            var responseMessage = new HttpResponseMessage
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(new IntentContent
+                    {
+                        Content = responseText
+                    })),
+            };
+
+            if (cookies != null)
+            {
+                responseMessage.Headers.Add("set-cookie", String.Join(";", cookies.Select(a => a.ToString())));
+            }
+
+            return responseMessage;
+        }
+
+        private static PersistentQueue<ApplicationParameter> BuildQueue()
+        {
+            return new PersistentQueue<ApplicationParameter>(null,
+                new Session {Attributes = new Dictionary<string, object>()}, "key");
         }
     }
 }
